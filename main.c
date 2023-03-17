56a92bee3d4e5c5356c23c93eb5022043dd66702
Niccolo Tosato <niccolo.tosato@yahoo.it>
2023-03-18 00:06:12 +0100

    Fixed bug in MPI_Type_Vector

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mpi.h>
-#define N 15
+#define N 10000
+
 
 void print_matrix(double* A,int n_col) {
   //debug purpose: print a matrix
@@ -29,11 +30,22 @@ void print_matrix_square(double* A,int n_col) {
   }
 }
 
-void mat_mul(double* A, double* B, double* C, int n_col) {
+void mat_mul(double*  A, double*  B, double*  C, int n_col) {
   for(int i=0; i<n_col;++i) {
-    int row=i*N;
+    int  row=i*N;
     for(int j=0;j<n_col;++j){
       int idx=row+j;
+      for(int k=0;k<N;++k) {
+        C[idx]+=A[row+k]*B[k*n_col+j];
+      }
+    }
+  }
+}
+void mat_mul2(double* restrict A, double* restrict B, double* restrict C, int n_col) {
+  for(int i=0; i<n_col;++i) {
+    int register row=i*N;
+    for(int j=0;j<n_col;++j){
+      int register idx=row+j;
       for(int k=0;k<N;++k) {
         C[idx]+=A[row+k]*B[k*n_col+j];
       }
@@ -69,36 +81,39 @@ int main(int argc,char* argv[]) {
   double* C=malloc(N*n_col*sizeof(double));
   memset(C, 0, N*n_col);
 
-  double* buffer=malloc(N*n_col*sizeof(double));
+  double* buffer=malloc(N*N*sizeof(double));
 
 
   
 
   MPI_Datatype blocco;
-  MPI_Type_vector(n_col,n_col,n_col*(n_col-2),MPI_DOUBLE,&blocco);
+  MPI_Type_vector(n_col,n_col,N,MPI_DOUBLE,&blocco);
   MPI_Type_commit(&blocco);
 
 
   for(int p=0;p<procs;++p){
+    
+
       MPI_Allgather(B+n_col*p, 1, blocco,
                   buffer , n_col*n_col, MPI_DOUBLE,
                   MPI_COMM_WORLD);
 
 #ifdef DEBUG
-    if(rank==2) {
+    if(rank==3) {
       printf("Stampo la colonna buffer\n");
-      print_matrix_transpose(buffer,n_col);
+      print_matrix(buffer,n_col);
     }
 #endif
 
-
+    mat_mul(A, buffer, C+n_col*p, n_col);
 #ifdef DEBUG
-    if(rank==2){
+    if(rank==3){
       printf("Stampo la matrice C!\n");
       print_matrix(C, n_col);
     }
 #endif
-    mat_mul(A, buffer, C+n_col*p, n_col);
+
+
   }
 
   double* C_final=malloc(N*N*sizeof(double));
@@ -111,11 +126,12 @@ int main(int argc,char* argv[]) {
              MPI_DOUBLE,
              0,
              MPI_COMM_WORLD);
-
+#ifdef DEBUG
   if(rank==0){
     printf("FINALEEEE \n");
     print_matrix(C_final,N);
   }
+  #endif
   MPI_Finalize();
 
 }

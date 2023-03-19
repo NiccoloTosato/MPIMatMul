ff75488e887d0feab817b3f8b253efe93a0b585f
Niccolo Tosato <niccolo.tosato@yahoo.it>
2023-03-19 19:19:50 +0100

    Finally working !

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <cblas.h>
 
 #ifndef N
-   #define N 18
+   #define N 21
 #endif
 
 
@@ -19,6 +19,7 @@ void print_matrix(double* A,int n_col) {
 }
 
 void print_matrix_transpose(double* A,int n_col) {
+  //debug purpose: print a matrix
   for(int i=0;i<N;++i){
     for(int j=0;j<n_col;++j)
       printf("%3.3g ",A[j+i*n_col]);
@@ -27,6 +28,7 @@ void print_matrix_transpose(double* A,int n_col) {
 }
 
 void print_matrix_square(double* A,int n_col) {
+  //debug purpose: print a matrix
   for(int i=0;i<n_col;++i){
     for(int j=0;j<n_col;++j)
       printf("%3.3g ",A[j+i*n_col]);
@@ -34,8 +36,9 @@ void print_matrix_square(double* A,int n_col) {
   }
 }
 
-void mat_mul(double* restrict A, double* restrict B, double* restrict C, int n_col) {
-  for(int i=0; i<n_col;++i) {
+void mat_mul(double* restrict A, double* restrict B, double* restrict C, int n_col,int n_fix) {
+
+  for(int i=0; i<n_fix;++i) {
     int register row=i*N;
     for(int j=0;j<n_col;++j){
       int register idx=row+j;
@@ -46,30 +49,53 @@ void mat_mul(double* restrict A, double* restrict B, double* restrict C, int n_c
   }
 }
 
+
 void init_mat(double* A, int n_elem,int offset){
+  //init the matrix sequentially
   for(int i=0;i<n_elem;++i)
     A[i]=i+offset;
 }
 void rank_mat(double* A, int n_elem,int rank){
+  //init the matrix with the value of the rank
   for(int i=0;i<n_elem;++i)
     A[i]=rank;
 }
 
 int calculate_col(int tot_col,int procs,int rank) {
+  //calculate how many row belong to the current rank
   return (rank < tot_col % procs) ? tot_col/procs +1 : tot_col/procs;
 }
 
 void set_recvcout(int* recvcount, int iter, int procs){
+  //set the recv_count array 
   int current=calculate_col(N,procs,iter);
   for(int p=0;p<procs;++p){
     recvcount[p]=calculate_col(N,procs,p)*current;
   }
+
 }
 
 void set_displacement(int* displacement,const int* recvcount,int procs) {
+  //calculate the displacement array using the recv_count array
   displacement[0]=0;
   for(int p=1;p<procs;++p)
-    displacement[p]=displacement[p-1]+recvcount[p];
+    displacement[p]=displacement[p-1]+recvcount[p-1];
+}
+
+int calculate_offset(int procs,int tot_col,int iter) {
+  //calculate the offset for each block
+  int n_resto = (tot_col / procs) + 1;
+  int n_normale = (tot_col / procs);
+  int diff = iter - (tot_col % procs);
+    return (iter < tot_col % procs) ? n_resto*iter  : n_resto * (tot_col % procs) + n_normale * diff ;
+}
+
+
+void extract(double* destination ,double*source,int n_fix,int n_col) {
+  //linearize the block
+  for(int line=0;line<n_fix;++line) {
+    memcpy( destination+n_col*line,source+N*line, n_col*sizeof(double));
+  }
 }
 
 int main(int argc,char* argv[]) {
@@ -77,7 +103,6 @@ int main(int argc,char* argv[]) {
   int procs,rank;
   MPI_Comm_size(MPI_COMM_WORLD,&procs);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  MPI_Barrier(MPI_COMM_WORLD);
 
   //dimensione locale per il blocco relativo al rank, non cambiera mai, se sono uno dei primi mi becco il resto
   int n_fix=(rank < N % procs) ? N/procs +1 : N/procs;
@@ -85,13 +110,6 @@ int main(int argc,char* argv[]) {
   int n_buffer= ( (N%procs) >= 1) ? (N/procs)+1 : N/procs ;
   //ad ogni iterazione verra' rivisto il numero di colonne
   int n_col;
-
-#ifdef DEBUG3
-  printf("rank %d n_fix %d, n_buffer %d\n",rank,n_fix,n_buffer);
-  fflush(stdout);
-  MPI_Barrier(MPI_COMM_WORLD);
-#endif
-
 
   double* A=malloc(N*n_fix*sizeof(double));
   //memset(A, 0, N*n_col);
@@ -104,67 +122,59 @@ int main(int argc,char* argv[]) {
   double* C=malloc(N*n_fix*sizeof(double));
   memset(C, 0, N*n_fix);
 
+  //allocate the buffer, it use the larger n_col possible
   double* buffer=malloc(N*n_buffer*sizeof(double));
+  //allocate the buffer to linearize the block
+  double* square=malloc(n_buffer*n_buffer*sizeof(double));
 
-  MPI_Datatype blocco;
-
+  //allocate displacement ad recvout array
   int* displacement = malloc(procs*sizeof(int));
   int* recvcount = malloc(procs*sizeof(int));
 
-  #ifdef SUPER
-  if(rank==0){
-    for(int pp=0;pp<procs;++pp){
-      int current=calculate_col(N,procs,pp);
-      printf("IT %d , n_col %d : ",pp,current);
-      set_recvcout(recvcount,pp,procs);
-      set_displacement(displacement,recvcount,procs);
-      for(int p=0;p<procs;++p){
-        printf(" %d|%d ",recvcount[p],displacement[p]);
-      }
-      printf("\n");
-    }
-  }
-  return 0;
-  #endif
-    for(int p=0;p<procs;++p){
-    //numero di colonne all'iterazione corrente
 
-      n_col=calculate_col(N,procs,p);
+  //MPI_Datatype blocco;
 
-    MPI_Type_vector(n_fix,n_col,N,MPI_DOUBLE,&blocco);
-    MPI_Type_commit(&blocco);
+  for(int p=0;p<procs;++p){
     
-    MPI_Allgather(B+n_col*p, 1, blocco,
-                  buffer , n_col*n_col, MPI_DOUBLE,
-                  MPI_COMM_WORLD);
-#ifdef DEBUG2
-    if(rank==3) {
-      printf("Stampo la colonna buffer\n");
-      print_matrix(buffer,n_col);
-    }
-#endif
+    //numero di colonne all'iterazione corrente
+    n_col=calculate_col(N,procs,p);
+    set_recvcout(recvcount,p,procs);
+    set_displacement(displacement,recvcount,procs);
 
+    //MPI_Type_vector(n_buffer, n_buffer, N, MPI_DOUBLE, &blocco);
+    //MPI_Type_commit(&blocco);
 
-    mat_mul(A, buffer, C+n_col*p, n_col);
-    //cblas_dgemm ( CblasRowMajor, CblasNoTrans, CblasNoTrans , n_col , n_col , N , 1.0 , A , N , buffer , n_col , 0.0 ,  C+n_col*p, N );
+    int offset=calculate_offset(procs,N,p);
 
-#ifdef DEBUG2
-    if(rank==3){
-      printf("Stampo la matrice C!\n");
-      print_matrix(C, n_col);
-    }
-#endif
+    
+    //MPI_Type_size(blocco, &size);
+    
+    
+    extract(square,B+offset,n_fix,n_col);
 
+    MPI_Allgatherv( square , n_col*n_fix, MPI_DOUBLE,
+                    buffer ,recvcount,displacement,MPI_DOUBLE,MPI_COMM_WORLD);
+    
+    //MPI_Type_free(&blocco);
+
+    //mat_mul(A, buffer, C+offset, n_col, n_fix); 
+    cblas_dgemm ( CblasRowMajor, CblasNoTrans, CblasNoTrans , n_fix , n_col , N , 1.0 , A , N , buffer , n_col , 0.0 ,  C+offset, N );
 
   }
+  
 
+#ifdef DEBUG
   double* C_final=malloc(N*N*sizeof(double));
-  MPI_Barrier(MPI_COMM_WORLD);
-  MPI_Gather(C,
-             N*n_col,
+  for(int p=0;p<procs;++p){
+    recvcount[p]=N * calculate_col(N,procs,p);
+  }
+#endif  
+  set_displacement(displacement,recvcount,procs);
+  MPI_Gatherv(C,
+             N*n_fix,
              MPI_DOUBLE,
              C_final,
-             N*n_col,
+              recvcount,displacement,
              MPI_DOUBLE,
              0,
              MPI_COMM_WORLD);
